b7ee41cc5bcd63dbc61abb925cd3f956d2e7a527
djatkdgus789 <djatkdgus789@github.com>
2020-11-20 17:14:07 +0900

    change more awesome

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,39 @@
-// Load plugins
-const browsersync = require("browser-sync").create();
-const gulp = require("gulp");
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const wait = require('gulp-wait');
+const babel = require('gulp-babel');;
+const rename = require('gulp-rename');
 
-// Copy third party libraries from /node_modules into /vendor
-gulp.task('vendor', function(cb) {
-
-  // Bootstrap
-  gulp.src([
-      './node_modules/bootstrap/dist/**/*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
-    ])
-    .pipe(gulp.dest('./vendor/bootstrap'))
-
-  // jQuery
-  gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
-    ])
-    .pipe(gulp.dest('./vendor/jquery'))
-
-  // jQuery Easing
-  gulp.src([
-      'node_modules/jquery.easing/*.js'
-    ])
-    .pipe(gulp.dest('vendor/jquery-easing'))
-
-  cb();
-
+gulp.task('scripts', function() {
+    return gulp.src('./js/scripts.js')
+        .pipe(plumber(plumber({
+            errorHandler: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        })))
+        .pipe(babel({
+          presets: [['@babel/env', {modules:false}]]
+        }))
+        .pipe(uglify({
+            output: {
+                comments: '/^!/'
+            }
+        }))
+        .pipe(rename({extname: '.min.js'}))
+        .pipe(gulp.dest('./js'));
 });
 
-// BrowserSync
-function browserSync(done) {
-  browsersync.init({
-    server: {
-      baseDir: "./"
-    }
-  });
-  done();
-}
+gulp.task('styles', function () {
+    return gulp.src('./scss/styles.scss')
+        .pipe(wait(250))
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
 
-// BrowserSync Reload
-function browserSyncReload(done) {
-  browsersync.reload();
-  done();
-}
-
-// Watch files
-function watchFiles() {
-  gulp.watch("./css/*", browserSyncReload);
-  gulp.watch("./**/*.html", browserSyncReload);
-}
-
-gulp.task("default", gulp.parallel('vendor'));
-
-// dev task
-gulp.task("dev", gulp.parallel(watchFiles, browserSync));
+gulp.task('watch', function() {
+    gulp.watch('./js/scripts.js', gulp.series('scripts'));
+    gulp.watch('./scss/styles.scss', gulp.series('styles'));
+});
